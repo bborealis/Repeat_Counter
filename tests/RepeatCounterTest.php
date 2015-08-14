ead7b29b34a7b1873bd33670e61ca3f5d1d10d2b
Brian Borealis <brian.borealis@gmail.com>
2015-08-14 11:25:40 -0700

    pass second test

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -13,6 +13,18 @@
 
             $this->assertEquals("1", $result);
         }
+
+        //2. User enters word: "a" and a string of words: "ba" and gets back "0". This checks that the app returns "0" for no matching words.
+        function test_countsRepeats_oneWordDifferent () {
+            $test_RepeatCounter = new RepeatCounter;
+            $word = "a";
+            $string_of_words = "ba";
+
+            $result = $test_RepeatCounter->countsRepeats($word, $string_of_words);
+
+            $this->assertEquals("0", $result);
+        }
+
     }
 
 
