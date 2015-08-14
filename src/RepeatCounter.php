e0798d8af60efccc1374b861bac18b58d892e6cc
Brian Borealis <brian.borealis@gmail.com>
2015-08-14 11:23:24 -0700

    pass first test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,6 +3,16 @@
 
         function countsRepeats($word, $string_of_words) {
 
+            $score = 0;
+
+            if($word == $string_of_words) {
+                $score = $score + 1;
+            }
+
+            $score = strval($score);
+            return $score;
+
+
         }
     }
 
